2f4f357801e85f7c59ae5e7f2c0fb48a0ef488a3
vigneshs-dev <vigneshkavery97@gmail.com>
2024-12-04 16:52:37 +0000

    Add docker-compose.yml file

--- Infrastructure/Modules/S3/main.tf
+++ Infrastructure/Modules/S3/main.tf
@@ -7,7 +7,7 @@
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket        = var.bucket_name
-  acl           = "private"
+  # acl           = "private"
   force_destroy = true
   tags = {
     Name = var.bucket_name
